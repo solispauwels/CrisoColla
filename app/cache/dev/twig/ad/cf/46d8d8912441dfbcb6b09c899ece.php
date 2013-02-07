2d36abb95cedbe61dc2ba619b0b64afa059d4783
esdras <esdras@esdras-RV420-RV520-RV720-E3530-S3530-E3420-E3520.(none)>
2013-02-07 15:08:24 +0100

    Hola

---
+++ app/cache/dev/twig/ad/cf/46d8d8912441dfbcb6b09c899ece.php
@@ -0,0 +1,39 @@
+<?php
+
+/* CrisoCollaTestBundle:Default:index.html.twig */
+class __TwigTemplate_adcf46d8d8912441dfbcb6b09c899ece extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "CrisoCollaTestBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
---
+++ src/CrisoColla/TestBundle/CrisoCollaTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CrisoColla\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CrisoCollaTestBundle extends Bundle
+{
+}
