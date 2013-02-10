9f2ffd22df2939542b91048635eab218ef33b8ca
Jorge Solis <jorgeluissolis@gmail.com>
2013-02-10 23:06:37 +0100

    Nothing important

--- src/CrisoColla/ThemeBundle/Controller/DefaultController.php
+++ src/CrisoColla/ThemeBundle/Controller/DefaultController.php
@@ -13,13 +13,21 @@ class DefaultController extends Controller
 	$request = $this->get('router')->match("/notheme/$path");
 
 	print_r($request);
+        
+	/**
+	 * @TODO prevent infinit recursion
+	 */
+	$response = $this->forward($request["_controller"], $request );
 	
-	$test=eval($request["_controller"]($request));
+
+	//$redirect = $this->redirect("notheme/$path");
+	
+	//$test=eval($request["_controller"]($request));
 
         //throw $this->createNotFoundException('The product does not exist');
 
 	//$content = $this->render('CrisoCollaThemeBundle:Default:index.html.twig', array('content' => $path));
 
-	return $this->render('CrisoCollaThemeBundle:Default:index.html.twig', array('content' => $path));
+	return $this->render('CrisoCollaThemeBundle:Default:index.html.twig', array('content' => $response));
     }
 }
