41604495ac5ad3e4a61931c966d878153a8f0ade
Jorge Solis <jorgeluissolis@gmail.com>
2013-02-17 23:15:37 +0100

    Update Theme bundle

---
+++ src/CrisoColla/ThemeBundle/Controller/ThemeController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace CrisoColla\ThemeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ThemeController extends Controller
+{
+	protected $theme;
+
+	public function __construct()
+	{
+		/**
+		 * @TODO Load from regions.
+		 */
+		$this->theme=array
+			(
+				//"header" => "",
+				//"left" => "",
+				//"right" => "",
+				//"content" => "",
+				//"footer" => "",	
+			);
+
+		$this->theme['theme']="Default";
+	}
+	
+	public function renderPathAction($path="home")
+	{
+		//$this->initialize();
+
+		$request = $this->get('router')->match("/notheme/$path");
+		//print_r($request);
+
+		if($this->getRequest()->attributes->get('_controller') != $request["_controller"])
+		{
+			$response = $this->forward($request["_controller"], $request);
+			
+			$this->theme["content"] = $response->getContent();
+		}
+		else
+		{
+			$this->theme["content"] = $this->render('CrisoCollaThemeBundle::error.html.twig', array('path' => $path))->getContent();
+		}
+
+		return $this->render('CrisoCollaThemeBundle:'.$this->theme['theme'].':layout.html.twig', $this->theme);
+	}
+}
