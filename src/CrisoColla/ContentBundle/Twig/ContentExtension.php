c465b1f1f2af0698de1f1016879bcefc080df6b4
Jorge Solis <jorgeluissolis@gmail.com>
2013-04-02 00:23:59 +0200

    timeAgo twig extention update

--- src/CrisoColla/ContentBundle/Twig/ContentExtension.php
+++ src/CrisoColla/ContentBundle/Twig/ContentExtension.php
@@ -20,22 +20,31 @@ class ContentExtension extends \Twig_Extension
 
     function timeAgo($start)
     {
-        $end = new \DateTime('now');
-
+        $end = new \DateTime("now");
+        
         $interval = $start->diff($end);
 
-        $formats = array("%y", "%m", "%W", "%d", "%G", "%i", "%s");
-        $translation["singular"] = array("%y" => "year", "%m" => "month", "%W" => "week", "%d" => "day", "%G" => "hour", "%i" => "minute", "%s" => "second");
-        $translation["plural"] = array("%y" => "years", "%m" => "months", "%W" => "weeks", "%d" => "days", "%G" => "hours", "%i" => "minutes", "%s" => "seconds");
+        $formats = array("%Y", "%m", "%W", "%d", "%H", "%i", "%s");
+        $translation["singular"] = array("%Y" => "year", "%m" => "month", "%W" => "week", "%d" => "day", "%H" => "hour", "%i" => "minute", "%s" => "second");
+        $translation["plural"] = array("%Y" => "years", "%m" => "months", "%W" => "weeks", "%d" => "days", "%H" => "hours", "%i" => "minutes", "%s" => "seconds");
 
         foreach($formats as $format)
         {
-            if($interval->format($format)>0)
+            if($format == "%W") //fix for week that does not exist in DataInterval obj
+            {
+                $i = round($interval->format("%d") / 8);
+            }
+            else
+            {
+                $i = ltrim($interval->format($format), "0");
+            }
+
+            if($i>0)
             {
                 return $this->translator->transChoice(
                     "%count% ".$translation["singular"][$format]." ago|%count% ".$translation["plural"][$format]." ago",
-                    $interval->format($format),
-                    array('%count%' => $interval->format($format)),
+                    $i,
+                    array('%count%' => $i),
                     "content"
                 );
             }
